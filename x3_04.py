47a24786596287a286a42a9703897e7e9445f591
Junichi Kawahara <kawahara-j@hi-ad.jp>
2019-02-21 10:41:48 +0900

    Second Commit

--- x3_04.py
+++ x3_04.py
@@ -1,8 +1,8 @@
 #! /usr/local/bin/python3
 
 """
-x3_03.py
-1~40を表示するスクリプト その3
+x3_04.py
+1~40を表示するスクリプト その4
 Copyright (C) 2019 J.Kawahara
 2019.2.21 J.Kawahara 新規作成
 """
@@ -25,17 +25,26 @@ def is_marked_number(n):
     return (n % 3) == 0
 
 
+def print_number(numbers):
+    """表示する文字列を生成するジェネレータ
+
+    numbers : list(int)
+        処理対象の数値のリスト
+    """
+    for n in numbers:
+        mark = '*' if is_marked_number(n) else ''
+        yield str(n) + mark
+
+
 def print_numbers():
     """1〜40の数字を表示する
 
-    while ステートメントで実装
+    ジェネレータを使って実装
     """
 
-    n = 1
-    while n <= 40:
-        mark = '*' if is_marked_number(n) else ''
-        print(str(n) + mark)
-        n += 1
+    for line in print_number(range(1, 40 + 1)):
+        print(line)
+
 
 if __name__ == '__main__':
     print_numbers()
